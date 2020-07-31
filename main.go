d067becfd2df4b065a9fc643483b5cb76c890c26
Ravi Wu <raviwu@gmail.com>
2020-07-31 11:49:30 +0800

    Serve file from server

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", dog)
+	http.HandleFunc("/dog.jpg", dogPic)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -14,6 +16,17 @@ func dog(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	io.WriteString(w, `
-	<img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Bvdb-bassethound1.jpg">
+	<img src="/dog.jpg">
 	`)
+}
+
+func dogPic(w http.ResponseWriter, req *http.Request) {
+	f, err := os.Open("dog.jpg")
+	if err != nil {
+		http.Error(w, "file not found", 404)
+		return
+	}
+	defer f.Close()
+
+	io.Copy(w, f)
 }
